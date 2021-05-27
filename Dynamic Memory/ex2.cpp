27515769c4925db86625aa802e8a6b46a85a2d54
davide136 <davide136@gmail.com>
2021-05-28 00:04:40 +0200

    Stack delete OK

--- Dynamic Memory/ex2.cpp
+++ Dynamic Memory/ex2.cpp
@@ -30,39 +30,58 @@ nodo* build1(int m)
 
 //PRE=(Lista(L) ben formata, vLista(L)=Lista(L))
 nodo* del1(nodo* L, int z){
-    if(!L)
-        return 0;
-    int val = L->info;
-    if(length(L)==0)
-        return 0;
-    else if(length(L)>0 && val != z)
-        return new nodo(val, del1(L->next,z));
-    else return new nodo(L->next->info, del1(L->next->next, z));
+    if(!L->next){
+        if(L->info==z)
+            return 0;
+        else
+            return new nodo(L->info, 0);
+    }
+    if(L->info==z)
+        return del1(L->next,z);
+    return new nodo(L->info, del1(L->next, z));
 }
 //POST=(restituisce una lista composta dai nodi di vLista(L)che non hanno campo info=z nell’ordine che hanno in vLista(L)) && (dealloca gli altri nodidi vLista(L))
 
 //PRE=(LIsta(L) ben formata, non vuota,e L->info != z, sia vLista(L)=Lista(L))
 void del2(nodo*L, int z){
-    if(!L)
-        return;
-    else if(length(L)>0 && L->info != z)
-        del2(L->next, z);
-    else if(L->info==z){
-        L->info = L->next->info;
-        L->next = L->next->next;
-        del2(L,z);
+    if(!L->next){   
+        if(L->info==z)
+            delete L;
+        else
+            return;
     }
-    else{ 
-        delete L;
+    else{
+        if(L->info==z){
+            L->info = L->next->info;
+            L->next = L->next->next;
+        }
+        if(L->next->info==z){
+            L->next=L->next->next;
+            del2(L,z);
+        }
+        else
+            del2(L->next,z);
+        
     }
-
-}
-//POST=(Lista(L) è ottenuta da vLista(L) eliminando i nodi con info=z e tenendo gli altri mantenendo il loro ordine) &&(i nodi con info=z sono deallocati)
+    
+}//POST=(Lista(L) è ottenuta da vLista(L) eliminando i nodi con info=z e tenendo gli altri mantenendo il loro ordine) &&(i nodi con info=z sono deallocati)
 
 //PRE=(Lista(L) ben formata, vLista(L)=Lista(L))
 void del3(nodo*&L, int z){
-   
-
+    if(!L->next){
+        if(L->info==z)
+            L=0;
+        else   
+            return;
+    }
+    else{
+        if(L->info==z){
+            L = L->next;
+            del3(L,z);
+        }
+        else
+            del3(L->next,z);
+    }
 }
 //POST=(Lista(L) è ottenuto da vLista(L) eliminando i nodi con info=z e mantenendo gli altri nello stessoordine che avevano in vLista(L) && (i nodi eliminati sono deallocati)
 
