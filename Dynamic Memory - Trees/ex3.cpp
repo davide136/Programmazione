b82bc42bceb5ff1da254dd6a74f7d9e71889ad99
davide136 <davide136@gmail.com>
2021-06-03 15:08:00 +0200

    avanzo giusto ma la richiesta del post non è soddisfatta e non si capisce

--- Dynamic Memory - Trees/ex3.cpp
+++ Dynamic Memory - Trees/ex3.cpp
@@ -53,32 +53,30 @@ void sc(int*C)
 string tree_to_string_PRE(nodo* root){
   string result = "";
   if(root){
-    result = result + to_string(root->info) + " ";
+    result += to_string(root->info);
     result += tree_to_string_PRE(root->left);
     result += tree_to_string_PRE(root->right);
   }
   return result;
 }
 
-string tree_to_string_INF(nodo* root){
-  string result = "";
+void tree_to_string_INF(nodo* root, string *s){
   if(root){
-    result += tree_to_string_PRE(root->left);
-    result = result + to_string(root->info) + " ";
-    result += tree_to_string_PRE(root->right);
+    tree_to_string_INF(root->left, s);
+    *s += to_string(root->info);
+    tree_to_string_INF(root->right, s);
   }
-  return result;
 }
 
 /*PRE=(albero(r) ben formato,0<= n<=k, k>0)*/
 int stampaASPre(nodo*r, int n){
   string r_prefisso = tree_to_string_PRE(r);
-  for(int i=2;i<r_prefisso.length();i=i+2){
-    if(i%(2*n) == 0){
-      cout << r_prefisso[i-2]<<' ';
+  int resto = 0;
+  for(int i=1;i<r_prefisso.length();i++)
+    if(i%(n) == 0){
+      cout << r_prefisso[i-1]<<' ';
+      resto = r_prefisso.length() - i;
     }
-  }
-  int resto = r_prefisso.length()%(2*n)-1;
   return resto;
 }
 /*POST=(considerando I nodi di albero(r) in ordine prefisso, salta n nodi e poi 
@@ -89,13 +87,15 @@ stampa quello successivo e dopo ne salta k-1 e poi stampa il successivo, restitu
  /*PRE=(albero(r) ben formato,0<= n<=k, k>0)*/
 int stampaASInf(nodo*r, int n){
 
-  string infisso = tree_to_string_INF(r);
-  for(int i=2;i<infisso.length();i=i+2){
-    if(i%(2*n) == 0){
-      cout << infisso[i-2]<<' ';
+  string infisso = "";
+  tree_to_string_INF(r,&infisso);
+  
+  int resto = 0;
+  for(int i=1;i<infisso.length();i++)
+    if(i%n == 0){
+      cout << infisso[i-1]<<' ';
+      resto = infisso.length() - i;
     }
-  }
-  int resto = infisso.length()%(2*n)-1;
   return resto;
     
 }
